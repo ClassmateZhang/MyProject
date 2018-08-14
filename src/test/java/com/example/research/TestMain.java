08d45147311b081d28d59e98c83df0efd0fff1fb
chengxiang.zhang <chengxiang.zhang@juniorchina.com>
2018-08-14 13:51:08 +0800

    bootstrap项目的创建

---
+++ src/test/java/com/example/research/TestMain.java
@@ -0,0 +1,14 @@
+package com.example.research;
+
+/**
+ * @ Author     ：zhangcx
+ * @ Date       ：Created in 9:27 2018/8/1
+ * @ Description：${description}
+ * @ Modified By：
+ * @Version: $version$
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("项目建设成功！！！");
+    }
+}

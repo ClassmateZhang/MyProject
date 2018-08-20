5403a384da871be9734bd16888c66ee6820573fd
chengxiang.zhang <chengxiang.zhang@juniorchina.com>
2018-08-20 13:33:20 +0800

    随机发红包完整流程

--- src/test/java/com/example/research/ResearchApplicationTests.java
+++ src/test/java/com/example/research/ResearchApplicationTests.java
@@ -12,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.*;
 
+import static com.example.research.TestMain.keyChanceMap;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ResearchApplicationTests {
@@ -21,6 +23,7 @@ public class ResearchApplicationTests {
 
 	@Autowired
 	private RedisTemplate redisTemplate;
+	ObjectMapper objectMapper = new ObjectMapper();
 
 	@Test
 	public void test() throws Exception {
@@ -40,21 +43,101 @@ public class ResearchApplicationTests {
 		mapLadder.put("ladder1","1700");
 		mapLadder.put("ladder2","300");
 		Map<String,String> mapAmount = new HashMap<String,String>();
-		mapAmount.put("amount1","8.88");
-		mapAmount.put("amount2","18.88");
+		mapAmount.put("ladder1","8.88");
+		mapAmount.put("ladder2","18.88");
 		ObjectMapper objectMapper = new ObjectMapper();
 		String ss = objectMapper.writeValueAsString(mapLadder);
 		System.out.println(ss);
 		String aa = objectMapper.writeValueAsString(mapAmount);
 		Map<String,String> ll =objectMapper.readValue(aa,Map.class);
 		System.out.println(aa);
-		System.out.println(ll+"--"+ll.get("amount1"));
+		System.out.println(ll+"--"+ll.get("ladder1"));
 		map.put("ladder",ss);
 		map.put("amount",aa);
 		redisTemplate.opsForHash().putAll("red_packet",map);
 		Map maps = redisTemplate.opsForHash().entries("red_packet");
 		System.out.println("完成！！！！"+maps);
 		// Assert.assertEquals("aa", operations.get("com.neo.f").getUserName());
+				/*Iterator iter = maps.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			Object key = entry.getKey();
+			Object val = entry.getValue();
+		}*/
 	}
 
+	/**
+	 * 发红包测试
+	 * @throws Exception
+	 */
+	@Test
+	public void test2() throws Exception{
+		for (int i = 0; i <10000 ; i++) {
+			//获得红包缓存及相关信息
+			Map maps = redisTemplate.opsForHash().entries("red_packet");
+			Integer count = Integer.parseInt((String)maps.get("count"));
+			if(count==0){
+				System.out.println("发奖结束！！！！！");
+				break;
+			}
+			Map ladderMap = objectMapper.readValue((String)maps.get("ladder"),Map.class);
+			Map amountMap = objectMapper.readValue((String)maps.get("amount"),Map.class);
+			redPacket(count,ladderMap,amountMap,maps);
+		}
+	}
+
+	public void redPacket(int counts,Map<String,String> ladder,Map<String,String> amount,Map<String,String> maps){
+		try {
+			Long time = System.currentTimeMillis();
+			Map<String, Integer> count = new HashMap<String, Integer>();
+			List<String> list = new ArrayList<>();
+			String item = null;
+			for (int i = 0; i < counts; i++) {
+				item = chanceSelect(ladder);
+				list.add(item);
+				if (count.containsKey(item)) {
+					count.put(item, count.get(item) + 1);
+				} else {
+					count.put(item, 1);
+				}
+			}
+			for (String id : count.keySet()) {
+				System.out.println(id + "\t出现了 " + count.get(id) + " 次");
+			}
+			Random rand = new Random();
+			int num = rand.nextInt(counts);
+			//数目-1
+			Integer ladders = Integer.parseInt(ladder.get(list.get(num)));
+			ladders--;
+			ladder.put(list.get(num),ladders+"");
+			counts--;
+			maps.put("ladder",objectMapper.writeValueAsString(ladder));
+			maps.put("count",counts+"");
+			System.err.println("最终发放的金额为："+amount.get(list.get(num))+" 耗时："+(System.currentTimeMillis()-time));
+			//存入缓存
+			redisTemplate.opsForHash().putAll("red_packet",maps);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static String chanceSelect(Map<String, String> ladder) {
+		if (ladder == null || ladder.size() == 0)
+			return null;
+		//总数量
+		Integer sum = 0;
+		for (String value : ladder.values()) {
+			sum += Integer.parseInt(value);
+		}
+		// 从1开始
+		Integer rand = new Random().nextInt(sum) + 1;
+		for (Map.Entry<String, String> entry : ladder.entrySet()) {
+			rand -= Integer.parseInt(entry.getValue());
+			// 选中
+			if (rand <= 0) {
+				String item = entry.getKey();
+				return item;
+			}
+		}
+		return null;
+	}
 }
--- src/test/java/com/example/research/TestMain.java
+++ src/test/java/com/example/research/TestMain.java
@@ -12,23 +12,32 @@ import java.util.*;
 public class TestMain {
     // String 可以为任意类型 也可以自定义类型
     static Map<String, Integer> keyChanceMap = new HashMap<String, Integer>();
+    //红包总数
+    static int counts= 2000;
     static {
-        keyChanceMap.put("aaa", 500);
-        keyChanceMap.put("bbb", 1500);
-        keyChanceMap.put("ccc", 2000);
-        keyChanceMap.put("ddd", 3000);
-        keyChanceMap.put("eee", 3000);
+        keyChanceMap.put("aaa8.88", 1700);
+        keyChanceMap.put("bbb18.88", 300);
     }
 
     public static void main(String[] args) {
+        for (int i = 1; i <=10000 ; i++) {
+            if(counts==0 || ((Integer)keyChanceMap.get("aaa8.88")==0 && (Integer)keyChanceMap.get("bbb18.88")==0)){
+                System.out.println("所有红包发放完成！！！");
+                break;
+            }
+            System.out.println("第"+i+"次发奖--》aaa8.88="+keyChanceMap.get("aaa8.88")+"--bbb18.88="+keyChanceMap.get("bbb18.88"));
+            redPacket();
+        }
+    }
+    public static void redPacket(){
+        Long time = System.currentTimeMillis();
         Map<String, Integer> count = new HashMap<String, Integer>();
         List<String> list = new ArrayList<>();
         String item = null;
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < counts; i++) {
             item = chanceSelect(keyChanceMap);
-            System.out.println("第"+(i+1)+"次选取为："+item);
+            //System.out.println("第"+(i+1)+"次选取为："+item);
             list.add(item);
-
             if (count.containsKey(item)) {
                 count.put(item, count.get(item) + 1);
             } else {
@@ -39,8 +48,12 @@ public class TestMain {
             System.out.println(id + "\t出现了 " + count.get(id) + " 次");
         }
         Random rand = new Random();
-        int num = rand.nextInt(10000);
-        System.out.print(num+"最终选择的随机数为："+list.get(num));
+        int num = rand.nextInt(counts);
+        //数目-1
+        Integer ladder = keyChanceMap.get(list.get(num));
+        keyChanceMap.put(list.get(num),ladder--);
+        counts--;
+        System.out.println(num+"最终选择的随机数为："+list.get(num)+" 耗时："+(System.currentTimeMillis()-time));
     }
     public static String chanceSelect(Map<String, Integer> keyChanceMap) {
         if (keyChanceMap == null || keyChanceMap.size() == 0)
@@ -53,11 +66,11 @@ public class TestMain {
         // 从1开始
         Integer rand = new Random().nextInt(sum) + 1;
         for (Map.Entry<String, Integer> entry : keyChanceMap.entrySet()) {
-            System.out.println("rand="+rand+"--value="+entry.getValue()+"-->rand -= entry.getValue()="+(rand-entry.getValue()));
+            //System.out.println("rand="+rand+"--value="+entry.getValue()+"-->rand -= entry.getValue()="+(rand-entry.getValue()));
             rand -= entry.getValue();
             // 选中
             if (rand <= 0) {
-                System.out.println(entry.getValue()+"被选中"+rand);
+                //System.out.println(entry.getValue()+"被选中"+rand);
                 String item = entry.getKey();
                 return item;
             }
